e6a1354e86b15d16c35c29f483e013caea02b58e
premchand <premch995@gmail.com>
2024-04-15 18:28:52 +0530

    added code

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hello from weather-app!');
+const axios = require('axios');
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3007;
+const API_KEY = '9053b61501b841aa240f60b301be5e07';
+
+async function fetchProduct(city){
+    try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+        return response.data;
+    } catch (error) {
+        throw new Error('Failed to fetch data');
+    }
+}
+
+app.get('/weather', async (req, res) => {
+    try {
+        const data  = await fetchProduct(req.query.city);
+        res.send(data);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
